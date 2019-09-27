04b4af6a0e25fdc13a2d92e6663a216e39639223
GreJangre <dlatjdwn8824@naver.com>
2019-09-27 17:04:29 +0900

    10171

---
+++ Baekjoon_Swift/10171.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Baekjoon_Swift
+//
+//  Created by 임성주 on 27/09/2019.
+//  Copyright © 2019 jangre. All rights reserved.
+//
+
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+import Foundation
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
